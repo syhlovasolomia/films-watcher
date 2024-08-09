83bbe90a80dbd370cb582f0abd57cc5193e71ba8
syhlovasolomia <syhlova.solomia@gmail.com>
2024-08-09 21:45:55 +0300

    added a list of films
    added numbering and alphabetical sorting, removed advertising from the right column, etc

--- js/script.js
+++ js/script.js
@@ -1,20 +1,6 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
-const movieDB = {
+const movieDataBase = {
     movies: [
         "Logan",
         "Justice League",
@@ -24,3 +10,27 @@ const movieDB = {
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'drama';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = "";
+
+movieDataBase.movies.sort();
+
+movieDataBase.movies.forEach((film, i) => {
+    movieList.innerHTML +=
+        `<li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+        </li>
+        `;
+});
